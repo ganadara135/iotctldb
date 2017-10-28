36bea464556ea3d7cab9a34cde234705978a927a
ganadara135 <ganadara135@gmail.com>
2017-10-28 18:55:05 +0900

    add mqtt

--- server.js
+++ server.js
@@ -53,3 +53,58 @@ port_param = 80;   // 클라우드용
 var server = app.listen(port_param, function(){
     console.log("Server has started on port ", port_param);
 });
+
+///////////////   mqtt connection with broker /////////////
+var mqtt = require('mqtt')
+var client  = mqtt.connect('mqtt://220.230.115.127')
+var CUBRIDforDoor = require('node-cubrid');
+const conCubridForDoor = CUBRIDforDoor.createCUBRIDConnection('1.255.54.209', 33000, 'dba', '', 'demodb');
+
+
+client.on('connect', function () {
+  client.subscribe('dooropen')
+  client.publish('dooropen', 'Hello mqtt from Blockchain Server')
+})
+
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+  if(message.toString() == '1') {
+    client.publish('dooropen', '2')
+    checkDoorReservation();
+  }
+//  client.end()
+})
+
+function checkDoorReservation(){
+
+  const promise = conCubridForDoor.connect()
+  .then(() => {
+    console.log('DB connection is established');
+
+    var sql = 'SELECT * from device';
+
+    console.log("sql ==> ", sql)
+    return conCubridForDoor.queryAllAsObjects(sql);
+  })
+  .then(response => {
+  // `result` is now an array of all row objects.
+//    assert(response)
+    console.log("response  ==> ", response);
+    const rowsCount = response.length;
+
+    if(rowsCount > 0){
+      console.log("rowsCount() > 0  ");
+    }else if(rowsCount == 0){
+      console.log("rowsCount() == 0  ");
+
+    }
+    conCubridForDoor.close();
+  })
+  .catch(err => {
+    // Handle the error.
+    console.log("err  ==> ", err);
+    conCubridForDoor.close();
+    throw err;
+  })
+}
